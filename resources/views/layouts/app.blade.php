56fa964a85b48cc2b71c361ad4f27c645f6ffe9a
Gavin Miles <milesg@heartsys.heartsystems.co.uk>
2018-09-24 16:51:15 +0100

    hex, hovers and mdbootstrap inclusion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,13 +8,15 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Heart Systems</title>
-
+<!--https://mdbootstrap.com/css/hover-effects/-->
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>    
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src='/js/parallax.min.js'></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/js/mdb.min.js"></script>
     
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
@@ -24,6 +26,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/css/mdb.min.css" rel="stylesheet">
 </head>
 <body>    
     <div id="app">        
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,31 +16,54 @@
 </div>
 
 <div class="container">
-    <div class="row" id="middleHome">    
+    <div class="row" id="middleHome">
         <div class="col">
-            <div class="hexagon" id="hex1">
-                <img src="/img/home-setup.png" style="display: block;border:none;margin: 0 auto;">
-                <strong><p>NO SET-UP</p></strong>
+            <div class="view overlay">
+                <div class="hexagon" id="hex1">
+                <img src="/img/home-setup.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
+                    <div class="mask flex-center rgba-pink-strong">
+                        <p class="white-text">Remote set up ensures a quick and simple transition from one system to another</p>
+                    </div>
+                    <p>NO SET-UP</p>
+                </div>    
             </div>
         </div>
+        
         <div class="col">
-            <div class="hexagon2" id="hex2">
-                <img src="/img/home-training.png" style="display: block;border:none;margin: 0 auto;">
-                <strong><p>MINIMAL TRAINING</p></strong>
+            <div class="view overlay">
+                <div class="hexagon2" id="hex1">
+                    <img src="/img/home-training.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
+                    <div class="mask flex-center rgba-purple-strong">
+                        <p class="white-text">Strong overlay</p>
+                    </div>
+                    <p>MINIMAL TRAINING</p>
+                </div>    
             </div>
         </div>
+        
         <div class="col">
-            <div class="hexagon3" id="hex3">
-                <img src="/img/home-disruption.png" style="display: block;border:none;margin: 0 auto;">
-                <strong><p>MINIMAL DISRUPTION</p></strong>
+            <div class="view overlay">
+                <div class="hexagon3" id="hex1">
+                    <img src="/img/home-disruption.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
+                    <div class="mask flex-center rgba-orange-strong">
+                        <p class="white-text">Strong overlay</p>
+                    </div>
+                    <p>MINIMAL DISRUPTION</p>
+                </div>    
             </div>
         </div>
+        
         <div class="col">
-            <div class="hexagon4" id="hex4">
-                <img src="/img/home-help.png" style="display: block;border:none;margin: 0 auto;">
-                <strong><p>HELP IS AT HAND</p></strong>
+            <div class="view overlay">
+                <div class="hexagon4" id="hex1">
+                    <img src="/img/home-help.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
+                    <div class="mask flex-center rgba-teal-strong">
+                        <p class="white-text">HELP IS AT HAND</p>
+                    </div>
+                    <p class="white-text">HELP IS AT HAND</p>
+                </div>    
             </div>
-        </div>  
+        </div>
     </div>
 </div>
 
