0b050e0bfbc959d24c3259a4799a4b5c177af1cb
Gavin Miles <milesg@heartsys.heartsystems.co.uk>
2018-09-24 15:49:43 +0100

    hexagons part1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
 <body>    
-    <div id="app" class="parallax-window" data-parallax="scroll" data-image-src="img/laptop.jpg">        
+    <div id="app">        
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel" id='navs'>
             <div class="container-fluid">
                 <nav class="navbar" id='navImage'>
@@ -99,7 +99,7 @@
             </div>
         </nav>
 
-        <main class="container-fluid">
+        <main>
             @yield('content')
         </main>
         <footer>  
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,49 @@
 @extends('layouts.app')
 @section('content')
-<br><br>
-<div id='intro' class='shadow-lg p-3 mb-5 bg-white rounded'>
-    <div class="panel-body">
-        <h2 id='headtext'>Run your business your way</h2>    
-        <h5>Proven. Simple. Flexible. Affordable</h5><br>
-        <p>Heart Systems are the leading supplier of software for small and medium sized Office Supplies companies in the UK and Ireland</p>
-        <div id='linkButton' ><a href='/pulseoffice'>Find out more</a></div>
+<div id="topBlock" class="container-fluid">
+    <div class="row">
+
+    <div class="col-lg">
+        
+    </div>
+        
+    <div class="col-lg" id="topBlockContent">
+        <h3>RUN YOUR BUSINESS YOUR WAY</h3>
+        <p>Proven. Simple. Flexible. Affordable.</p>
+        <p>Heart is the leading supplier of software for small and medium sized Office Supplies companies in the UK and Ireland.</p>                
+    </div>
     </div>    
 </div>
 
-<div id="homeContent" class='panel-default' >
-    <br><br>
-    <div id="homeContentPanel" class="panel-body">
-    <h2>Software for your office supplies business</h2>
-    <p>Manage your sales, ordering and invoicing with Pulse by Heart Systems.</p>
+<div class="container">
+    <div class="row" id="middleHome">    
+        <div class="col">
+            <div class="hexagon" id="hex1">
+                <img src="/img/home-setup.png" style="display: block;border:none;margin: 0 auto;">
+                <strong><p>NO SET-UP</p></strong>
+            </div>
+        </div>
+        <div class="col">
+            <div class="hexagon2" id="hex2">
+                <img src="/img/home-training.png" style="display: block;border:none;margin: 0 auto;">
+                <strong><p>MINIMAL TRAINING</p></strong>
+            </div>
+        </div>
+        <div class="col">
+            <div class="hexagon3" id="hex3">
+                <img src="/img/home-disruption.png" style="display: block;border:none;margin: 0 auto;">
+                <strong><p>MINIMAL DISRUPTION</p></strong>
+            </div>
+        </div>
+        <div class="col">
+            <div class="hexagon4" id="hex4">
+                <img src="/img/home-help.png" style="display: block;border:none;margin: 0 auto;">
+                <strong><p>HELP IS AT HAND</p></strong>
+            </div>
+        </div>  
     </div>
 </div>
 
-<div class="container" id="homeBoxContainer">
-    <div class="row">
-        <div class="col-sm" id="homeBox" style="background-color: #DA587D; margin-right: 50px;">
-            <img src="img/home-setup.png" id="homeBoxImage">
-            <strong><p>NO SET-UP </p></strong>
-        </div>
-        <div class="col-sm" id="homeBox" style="background-color: #8B6E95; margin-right: 50px;">
-            <img src="img/home-training.png" id="homeBoxImage">
-            <strong><p>MINIMAL TRAINING</p></strong>
-        </div>
-        <div class="col-sm" id="homeBox" style="background-color: #F2AA4D; margin-right: 50px;">
-            <img src="img/home-disruption.png" id="homeBoxImage">
-            <p>MINIMAL DISRUPTION</p>
-        </div>
-        <div class="col-sm" id="homeBox" style="background-color: #216A80;margin-right: 50px;">
-            <img src="img/home-help.png" id="homeBoxImage">
-            <p>HELP IS AT HAND</p>
-        </div>
-    </div>    
-</div>
 @endsection
 
     
-    <!--https://blackrockdigital.github.io/startbootstrap-business-casual/index.html-->   
-
