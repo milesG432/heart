1242027102cd0f89750543f2e842ba0342e06fcb
Gavin Miles <milesg@heartsys.heartsystems.co.uk>
2018-09-27 14:55:59 +0100

    MORE LOGIN STUFF

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -24,6 +24,7 @@ class userController extends Controller
                 {
                     $user = new User();
                     $result =  $user->checkUser($email, $password);
+                    
                 } 
                 else 
                 {
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use DB;
 
 
 
@@ -14,11 +15,43 @@ class User extends Authenticatable
     
     public function checkUser($email, $password)
     {
+        $errors = [
+            
+        ];
+        $login = [
+            
+        ];
         try
         {
-            var_dump($email);
+            if($email && $password)
+            {
+                $results = DB::select("SELECT * FROM user WHERE email = '" . $email . "' and password = '" . $password . "' LIMIT 1;");                
+                if(sizeof($results) == 1)
+                {                    
+                    $login['loggedIn'] = true;
+                    $login['user'] = $results[0] -> firstname;
+                    $login['accessLevel'] = $results[0] -> accessLevel;
+                }
+                else
+                {
+                    $errors['noUser'] = "There was no user located with the credentials provided. Please check your details and try again.";
+                }
+                if(sizeof($login) > 0)
+                {
+                    return $login;
+                }
+                elseif(sizeof($errors) > 0)
+                {
+                    return $errors;
+                }
+            }
+            else
+            {
+                $errors['Error finding login credentials. Please contact heart systems.'];
+            }
         } catch (Exception $ex) {
-
+            $errors['exception'] = $ex['message'];
+            return $errors;
         }
     }
    
