4b3f7412690b67c0dbac248bc45a3e0813978134
Gavin Miles <milesg@heartsys.heartsystems.co.uk>
2018-12-03 16:47:56 +0000

    cosmetics and content

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,85 +26,99 @@
 
     <br>
     <div class="row">
-        <div class="col">
-            <div class="card" style="width: 18rem;">
+        <div class="col-sm-12 col-lg-3">
+            <div class="card h-100" >
                 <img class="card-img-top" id="cardImg" src="/img/computer-cutout.png" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <br><h5 class="card-title">PulseOffice</h5>
                     <p class="card-text">PulseOffice has been core to Office Supplies dealers for over a decade, let us show you why.</p>
-                    <a href="/pulseoffice" class="btn btn-primary">Learn more</a>
+                    <a href="/pulseoffice" class="mt-auto  btn" style="background-color: #da587d">Learn more</a>
                 </div>
             </div>
-        </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
+        </div>        
+        <div class="col-sm-12 col-lg-3">
+            <div class="card h-100" >
                 <img class="card-img-top" id="cardImg" src="/img/heartbus.png" alt="Card image cap">
-                <div class="card-body">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">PulseStore</h5>
                     <p class="card-text">PulseStore provides an effective and user friendly web presence with a simple yet flexible interface.</p>
-                    <a href="/pulsestore" class="btn btn-primary">Learn more</a>
+                    <a href="/pulsestore" class="mt-auto  btn" style="background-color: #8b6e95">Learn more</a>
                 </div>
             </div>
         </div>
-
-        <div class="col">
-            <div class="card" style="width: 18rem;">
-
-                <div class="card-body">
+        <div class="col-sm-12 col-lg-3">
+            <div class="card h-100" >
+                <img class="card-img-top" id="cardImg" src="/img/wwear.jpeg" alt="Card image cap">
+                <div class="card-body d-flex flex-column">
+                    <br><h5 class="card-title">Pulse Workwear</h5>
+                    <p class="card-text">We can provide detailed and useful backoffice and web data for your workwear business.</p>
+                    <a href="/pulseoffice" class="mt-auto btn" style="background-color: #216a80"">Learn more</a>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-12 col-lg-3">
+            <div class="card h-100">
+                <div class="card-body d-flex flex-column">
                     <h5 class="card-title">Keep informed</h5>
                     <a class="twitter-timeline" data-width="454" data-height="320" href="https://twitter.com/Heart_Systems?ref_src=twsrc%5Etfw">Tweets by Heart_Systems</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                    <a href="https://twitter.com/heart_systems?lang=en" class="btn btn-primary">Follow</a>
+                    <a href="https://twitter.com/heart_systems?lang=en" class="mt-auto btn" style="background-color: #16A89D">Follow</a>
                 </div>
             </div>
         </div>
     </div>
-    <br>
-    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <div class="jumbotron" id="testimonial">
-                    <h3 class="display-4">Initial quote</h3>                    
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
-                    Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
-                    <hr>
-                    <p><i>John Marston</i></p>
-                    <strong><i>Western Stationery</i></strong>
+    <br>    
+    <div class="card">
+        <div class="card-header" style="text-align:center;background-color: #3D4049; color: #f2aa4d">
+      <h2>Testimonials</h2>    
+  </div>
+  <div class="card-body">  
+      <blockquote><h4><i>Dont just take our word for it, take the word of satisfied users....</i></h4></blockquote>
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <div class="jumbotron" id="testimonial">
+                        <h3 class="display-4">Initial quote</h3>                    
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
+                        Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
+                        <hr>
+                        <p><i>John Marston</i></p>
+                        <strong><i>Western Stationery</i></strong>
+                    </div>
                 </div>
-            </div>
-            <div class="carousel-item">
-                <div class="jumbotron" id="testimonial">
-                    <h3 class="display-4">Heart Systems rules!</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
-                    Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
-                    <hr>
-                    <p><i>Alex Murphy</i></p>
-                    <strong><i>Robocop academy</i></strong>
+                <div class="carousel-item">
+                    <div class="jumbotron" id="testimonial">
+                        <h3 class="display-4">Dogs R us!!</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
+                        Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
+                        <hr>
+                        <p><i>Alex Murphy</i></p>
+                        <strong><i>Robocop academy</i></strong>
+                    </div>
                 </div>
-            </div>
-            <div class="carousel-item">
-                <div class="jumbotron" id="testimonial">
-                    <h3 class="display-4">Gav is awesome</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
-                    Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
-                    <hr>
-                    <p><i>Mike Tyson</i></p>
-                    <strong><i>Peace and love Ltd</i></strong>
+                <div class="carousel-item">
+                    <div class="jumbotron" id="testimonial">
+                        <h3 class="display-4">Gav is awesome</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        Decathlon champion tom selleck nigel mansell albert einstein man markings, tom selleck nigel mansell hungarian chevron cripes village people albert einstein man markings dali dolor sit amet chevron decathlon champion.
+                        Sergeant major ian botham lady magnets hair trimmer? D’artagnan dolor ipsum boxing champion class definer funny walk tudor philosopher blue oyster bar challenge you to a duel.</p>
+                        <hr>
+                        <p><i>Mike Tyson</i></p>
+                        <strong><i>Peace and love Ltd</i></strong>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>    
+    </div>
 </div>
-
-
+        
+    </div>
 
 
 <div class="container">
     <div class="row" id="middleHome">
-        <div class="col">
+        <div class="col-lg-3">
             <div class="view overlay">
                 <div class="hexagon" id="hex1">
                     <img src="/img/home-setup.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
@@ -116,7 +130,7 @@
             </div>
         </div>
 
-        <div class="col">
+        <div class="col-lg-3">
             <div class="view overlay">
                 <div class="hexagon2" id="hex1">
                     <img src="/img/home-training.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
@@ -128,7 +142,7 @@
             </div>
         </div>
 
-        <div class="col">
+        <div class="col-lg-3">
             <div class="view overlay">
                 <div class="hexagon4" id="hex1">
                     <img src="/img/home-disruption.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
@@ -140,17 +154,17 @@
             </div>
         </div>
 
-        <!--        <div class="col">
-                    <div class="view overlay">
-                        <div class="hexagon4" id="hex1">
-                            <img src="/img/home-help.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
-                            <div class="mask flex-center rgba-teal-strong">
-                                <p class="white-text">With a combined total of over 50 years supporting or users we have seen it all and fixed it as well.</p>
-                            </div>
-                            <p class="white-text">HELP IS AT HAND</p>
-                        </div>    
+        <div class="col-lg-3">
+            <div class="view overlay">
+                <div class="hexagon5" id="hex1">
+                    <img src="/img/home-help.png" class="img-fluid " style="display: block;border:none;margin: 0 auto;">
+                    <div class="mask flex-center rgba-cyan-strong">
+                        <p class="white-text">With a combined total of over 50 years supporting or users we have seen it all and fixed it as well.</p>
                     </div>
-                </div>-->
+                    <p class="white-text">HELP IS AT HAND</p>
+                </div>    
+            </div>
+        </div>
     </div>
 </div>
 
